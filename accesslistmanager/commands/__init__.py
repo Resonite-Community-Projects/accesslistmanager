328cba0da4d3608b5bb197b2329242e072a0d97f
brodokk <brodokk@brodokk.space>
2023-03-18 14:48:12 +0100

    Add ability to send message to user based on a template

--- accesslistmanager/commands/__init__.py
+++ accesslistmanager/commands/__init__.py
@@ -1,4 +1,5 @@
 from .accesslist import AccessList
 from .searchuser import SearchUser
+from .sendmessage import SendMessage
 
-__all__ = [AccessList, SearchUser]
+__all__ = [AccessList, SearchUser, SendMessage]
